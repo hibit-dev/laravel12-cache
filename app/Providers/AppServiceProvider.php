4a81179b84684d99b947156a741477486882f87c
Tigran Mekinyan <tmekinyan@mail.ru>
2025-06-29 16:09:26 +0200

    Add tagged user repository bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Repository\TaggedUserRepositoryInterface;
+use App\Repository\TaggedUserRepository;
+
 use App\Repository\UserRepositoryInterface;
 use App\Repository\UserRepository;
 
@@ -12,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(TaggedUserRepositoryInterface::class, TaggedUserRepository::class);
     }
 
     public function boot(): void
